4f3d4d6f69254288436374714f32dd87c5aee610
vincent anjiri <vincentanjiri12@gmail.com>
2023-12-13 18:54:04 +0300

    Hoverboard

---
+++ script.js
@@ -0,0 +1,29 @@
+const container = document.getElementById('container')
+const colors = ['#e70c3c', '#8e04ad', '#3490db', '#e67e02', '#2ecc01']
+const SQUARES = 500
+
+for(let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseout', () => removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element) {
+   const color = getRandomColor()
+   element.style.background = color
+   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+   element.style.background = '#1d1d1d'
+   element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
